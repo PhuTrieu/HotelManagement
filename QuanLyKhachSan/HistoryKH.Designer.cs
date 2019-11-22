986cb86d208124737884ed7fe09faab208e3cd59
PhuTrieu <uyphu4@gmail.com>
2019-11-22 08:56:43 +0700

    First commit for QuanLyKhachSan

---
+++ QuanLyKhachSan/HistoryKH.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class HistoryKH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTenKH = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.lvwLS = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader13 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxLoaiphong = new System.Windows.Forms.ComboBox();
+            this.txtQuoctich = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(452, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ tên khách hàng";
+            // 
+            // txtTenKH
+            // 
+            this.txtTenKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenKH.Location = new System.Drawing.Point(634, 31);
+            this.txtTenKH.Name = "txtTenKH";
+            this.txtTenKH.Size = new System.Drawing.Size(290, 27);
+            this.txtTenKH.TabIndex = 1;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.Location = new System.Drawing.Point(500, 178);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(141, 65);
+            this.btnTimKiem.TabIndex = 2;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // btnXem
+            // 
+            this.btnXem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.Location = new System.Drawing.Point(749, 178);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(141, 65);
+            this.btnXem.TabIndex = 3;
+            this.btnXem.Text = "Xem";
+            this.btnXem.UseVisualStyleBackColor = true;
+            // 
+            // lvwLS
+            // 
+            this.lvwLS.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader7,
+            this.columnHeader10,
+            this.columnHeader11,
+            this.columnHeader13,
+            this.columnHeader6,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.lvwLS.FullRowSelect = true;
+            this.lvwLS.GridLines = true;
+            this.lvwLS.HideSelection = false;
+            this.lvwLS.Location = new System.Drawing.Point(12, 278);
+            this.lvwLS.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lvwLS.MultiSelect = false;
+            this.lvwLS.Name = "lvwLS";
+            this.lvwLS.Size = new System.Drawing.Size(1406, 461);
+            this.lvwLS.TabIndex = 50;
+            this.lvwLS.UseCompatibleStateImageBehavior = false;
+            this.lvwLS.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Tên khách hàng";
+            this.columnHeader1.Width = 136;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Số CMND/Hộ chiếu";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 147;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 6;
+            this.columnHeader3.Text = "Số phòng";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 95;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.DisplayIndex = 7;
+            this.columnHeader4.Text = "Ngày đến";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 95;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.DisplayIndex = 8;
+            this.columnHeader5.Text = "Ngày đi";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 95;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.DisplayIndex = 9;
+            this.columnHeader6.Text = "Loại phòng";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 104;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Số ngày ở";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader8.Width = 78;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Thành tiền";
+            this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader9.Width = 98;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Giới tính";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Tuổi";
+            this.columnHeader10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Quốc tịch";
+            this.columnHeader11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader11.Width = 115;
+            // 
+            // columnHeader13
+            // 
+            this.columnHeader13.Text = "Số điện thoại";
+            this.columnHeader13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader13.Width = 112;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(452, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "Loại phòng";
+            // 
+            // cbxLoaiphong
+            // 
+            this.cbxLoaiphong.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxLoaiphong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxLoaiphong.FormattingEnabled = true;
+            this.cbxLoaiphong.Items.AddRange(new object[] {
+            "Đơn",
+            "Đôi",
+            "Cao cấp"});
+            this.cbxLoaiphong.Location = new System.Drawing.Point(634, 73);
+            this.cbxLoaiphong.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbxLoaiphong.Name = "cbxLoaiphong";
+            this.cbxLoaiphong.Size = new System.Drawing.Size(290, 28);
+            this.cbxLoaiphong.TabIndex = 52;
+            // 
+            // txtQuoctich
+            // 
+            this.txtQuoctich.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuoctich.Location = new System.Drawing.Point(634, 119);
+            this.txtQuoctich.Name = "txtQuoctich";
+            this.txtQuoctich.Size = new System.Drawing.Size(290, 27);
+            this.txtQuoctich.TabIndex = 54;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(452, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 25);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Quốc tịch";
+            // 
+            // HistoryKH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1430, 750);
+            this.Controls.Add(this.txtQuoctich);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxLoaiphong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lvwLS);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTenKH);
+            this.Controls.Add(this.label1);
+            this.Name = "HistoryKH";
+            this.Text = "Thông tin lịch sử khách hàng";
+            this.Load += new System.EventHandler(this.HistoryKH_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTenKH;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.ListView lvwLS;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.ColumnHeader columnHeader13;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxLoaiphong;
+        private System.Windows.Forms.TextBox txtQuoctich;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ QuanLyKhachSan/HistoryKH.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class HistoryKH : Form
+    {
+        FormManager frmmng = new FormManager();
+        private List<HistoryKH> arrLS;
+
+        private List<CKhachHang> arrKH;
+        private List<CDatPhong> arrDP;
+
+        private int i = -1;
+        public HistoryKH()
+        {
+            InitializeComponent();
+        }
+
+        private void HistoryKH_Load(object sender, EventArgs e)
+        {
+            arrLS = new List<HistoryKH>();
+        }
+
+        public void hienthi()
+        {
+            lvwLS.Items.Clear();
+            foreach (CHistory ls  in arrLS)
+            {
+                ListViewItem li = lvwLS.Items.Add(ls.Dp.Kh.Hoten);
+                li.SubItems.Add(ls.Dp.Kh.CMND.ToString());
+                if (ls.Kh.Gioitinh == true)
+                {
+                    li.SubItems.Add("Nam");
+                }
+                else li.SubItems.Add("Nu");
+                li.SubItems.Add(ls.Kh.Tuoi.ToString());
+                li.SubItems.Add(ls.Kh.Quoctich);
+                li.SubItems.Add(ls.Kh.Sdt.ToString());
+                li.SubItems.Add(ls.Dp.Phong.Loaiphong);
+                li.SubItems.Add(ls.Dp.Phong.Sophong.ToString());
+                li.SubItems.Add(ls.Dp.Ngayden.ToShortDateString());
+                li.SubItems.Add(ls.Dp.Ngaydi.ToShortDateString());
+                li.SubItems.Add(ls.Dp.SoNgayO().ToString());
+                li.SubItems.Add(ls.Dp.ThanhTien().ToString());
+            }
+        }
+    }
+}
